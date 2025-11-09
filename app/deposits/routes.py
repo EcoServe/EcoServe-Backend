cea70ca95daf31d48102a3e6bb58ef37f2e96e37
Abhay Bansal <abhaybansal1920@gmail.com>
2025-11-09 18:24:10 +0530

    Refactor create_deposit to use JSON input

--- app/deposits/routes.py
+++ app/deposits/routes.py
@@ -1,22 +1,20 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify
 from ..extensions import db
-from ..models import DropBox, ItemType, Deposit, CreditLedger
 from .service import add_deposit
 
 bp = Blueprint("deposits", __name__)
 
-@bp.get("/d/<qr_token>")
-def deposit_form(qr_token):
-    box = DropBox.query.filter_by(qr_token=qr_token).first_or_404()
-    items = ItemType.query.filter_by(active=True).all()
-    return render_template("deposit.html", box=box, items=items)
-
 @bp.post("/api/deposits")
 def create_deposit():
-    data = request.form or request.json
-    user_id = int(data.get("user_id", 0))
-    drop_box_id = int(data.get("drop_box_id"))
-    item_type_id = int(data.get("item_type_id"))
-    qty = int(data.get("qty"))
-    dep, credits = add_deposit(user_id, drop_box_id)
-    return {"id": dep.id, "credits_awarded": credits}
+    data = request.get_json() or {}
+
+    user_id      = data.get("user_id")
+    drop_box_id  = data.get("drop_box_id")
+    item_type_id = data.get("item_type_id")
+    qty          = data.get("qty")
+
+    if not all([user_id, drop_box_id, item_type_id, qty]):
+        return jsonify({"error": "missing_fields"}), 400
+
+    dep, credits = add_deposit(user_id, drop_box_id, item_type_id, qty)
+    return jsonify({"id": dep.id, "credits": credits}), 201
